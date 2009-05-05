638bff8a0e2c0d754529c49b58d484160e5de5e3
Keith Donald <kdonald@vmware.com>
2009-05-05 17:30:38 +0000

    app deploys and runs - initial commit

---
+++ org.springframework.samples.petclinic/src/main/java/org/springframework/samples/petclinic/owner/StubOwnerRepository.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.owner;
+
+import java.util.Collection;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StubOwnerRepository implements OwnerRepository {
+
+	public Collection<Owner***REMOVED*** findOwnersByLastName(String lastName) {
+		return null;
+	***REMOVED***
+
+	public Owner getOwner(Long id) {
+		return null;
+	***REMOVED***
+
+	public void saveOwner(Owner owner) {
+	***REMOVED***
+
+***REMOVED***
