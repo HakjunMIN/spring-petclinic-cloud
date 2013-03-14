09d391c567ebde3be34941b8ac1af6ec58df845c
Mic <misvy@vmware.com>
2013-03-14 18:04:36 +0800

    simplifying access to Integer

--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcOwnerRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcOwnerRepositoryImpl.java
@@ -106,7 +106,7 @@ public class JdbcOwnerRepositoryImpl implements OwnerRepository {
                     ParameterizedBeanPropertyRowMapper.newInstance(Owner.class)
             );
   ***REMOVED*** catch (EmptyResultDataAccessException ex) {
-            throw new ObjectRetrievalFailureException(Owner.class, new Integer(id));
+            throw new ObjectRetrievalFailureException(Owner.class, id);
   ***REMOVED***
         loadPetsAndVisits(owner);
         return owner;
