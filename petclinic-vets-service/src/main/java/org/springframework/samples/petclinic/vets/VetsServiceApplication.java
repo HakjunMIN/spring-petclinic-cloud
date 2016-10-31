75573f61c3c25d53145851a68bf70f1b2740537b
Maciej Szarlinski <mszarlinski@bravurasolutions.com>
2016-10-31 14:50:14 +0100

    Create Spring Boot application for each bounded context

---
+++ petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic.vets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+***REMOVED***
+***REMOVED***
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetsServiceApplication.class, args);
+	***REMOVED***
+***REMOVED***
