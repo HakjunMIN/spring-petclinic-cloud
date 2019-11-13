62ff0d0ebf703331813abf41af78afd670b64e06
Antoine Rey <antoine.rey@gmail.com>
2019-11-13 18:31:52 +0100

    Replace Ribbon by spring-cloud-reactive-loadbalancer and Webflux WebClient

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/ApiGatewayController.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/ApiGatewayController.java
@@ -15,21 +15,19 @@
  */
 package org.springframework.samples.petclinic.api.boundary.web;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.samples.petclinic.api.application.CustomersServiceClient;
-import org.springframework.samples.petclinic.api.dto.OwnerDetails;
 import org.springframework.samples.petclinic.api.application.VisitsServiceClient;
-import org.springframework.samples.petclinic.api.dto.VisitDetails;
+import org.springframework.samples.petclinic.api.dto.OwnerDetails;
+import org.springframework.samples.petclinic.api.dto.Visits;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
-
-import static java.util.Collections.emptyList;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author Maciej Szarlinski
@@ -44,14 +42,24 @@ public class ApiGatewayController {
     private final VisitsServiceClient visitsServiceClient;
 
     @GetMapping(value = "owners/{ownerId***REMOVED***")
-    public OwnerDetails getOwnerDetails(final @PathVariable int ownerId) {
-        final OwnerDetails owner = customersServiceClient.getOwner(ownerId);
-        supplyVisits(owner, visitsServiceClient.getVisitsForPets(owner.getPetIds()));
-        return owner;
+    public Mono<OwnerDetails***REMOVED*** getOwnerDetails(final @PathVariable int ownerId) {
+        return customersServiceClient.getOwner(ownerId)
+            .flatMap(owner -***REMOVED***
+                visitsServiceClient.getVisitsForPets(owner.getPetIds())
+                    .map(addVisitsToOwner(owner))
+            );
+
     ***REMOVED***
 
-    private void supplyVisits(final OwnerDetails owner, final Map<Integer, List<VisitDetails***REMOVED******REMOVED*** visitsMapping) {
-        owner.getPets().forEach(pet -***REMOVED***
-            pet.getVisits().addAll(Optional.ofNullable(visitsMapping.get(pet.getId())).orElse(emptyList())));
+    private Function<Visits, OwnerDetails***REMOVED*** addVisitsToOwner(OwnerDetails owner) {
+        return visits -***REMOVED*** {
+            owner.getPets()
+                .forEach(pet -***REMOVED*** pet.getVisits()
+                    .addAll(visits.getItems().stream()
+                        .filter(v -***REMOVED*** v.getPetId() == pet.getId())
+                        .collect(Collectors.toList()))
+                );
+            return owner;
+  ***REMOVED***;
     ***REMOVED***
 ***REMOVED***
